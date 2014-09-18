017c9172e7f7485819a7a03999103b8dcc8ec3d5
zhongjuntian <zhongjun.tian@utdallas.edu>
2014-09-18 02:29:20 -0500

    1st test

---
+++ 000_Test_01/src/Myclass.java
@@ -0,0 +1,11 @@
+//Given an array of integers, every element appears three times except for one. Find that single one.
+public class Myclass {
+	public static int singleNumber(int[] A) {
+		
+		return 0;
+    }
+	
+	public static void main(String[] args){
+		
+	}
+}
