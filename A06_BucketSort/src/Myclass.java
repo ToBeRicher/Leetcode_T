465f59c8976f18651f1464f1682ebb621e03268e
zhongjuntian <zhongjun.tian@utdallas.edu>
2014-09-25 03:00:07 -0500

    Nothing

---
+++ A06_BucketSort/src/Myclass.java
@@ -0,0 +1,34 @@
+//桶排序
+//09/24/2014
+//田忠骏
+public class Myclass {
+	
+	public static void main(String[] args){		
+		int N=1000;
+		double[]A = Myclass.getRamdomNumberInt(N);//产生0-100范围的数
+		
+		long startT = System.nanoTime();  
+		
+		Myclass.Sort(A,N);
+		
+	    long endT = System.nanoTime();  
+		
+		for(int i=0;i<N;i++)
+			System.out.println(A[i]);
+	
+	    System.out.println(N+"个数排序用时：" + (double)(endT - startT)/1000000+"毫秒");  
+	}
+	
+	public static void Sort(double[] array,int length){
+		
+	}
+
+	public static double[] getRamdomNumberInt(int N) {
+		N=Math.abs(N);
+		double []A= new double[N];
+		for(int i=0;i<N;i++){
+			A[i]=(int)(Math.random()*100);
+		}
+		return A;
+    }
+}
