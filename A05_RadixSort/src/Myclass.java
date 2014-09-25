798f5e211f6a453575f4a72ba0eb132a501050b0
zhongjuntian <zhongjun.tian@utdallas.edu>
2014-09-25 02:57:12 -0500

    0924

--- A05_RadixSort/src/Myclass.java
+++ A05_RadixSort/src/Myclass.java
@@ -1,11 +1,11 @@
-//归并排序
-//09/21/2014
+//基数排序
+//09/24/2014
 //田忠骏
 public class Myclass {
 	
 	public static void main(String[] args){		
 		int N=1000;
-		double[]A = Myclass.getRamdomNumber(N);
+		double[]A = Myclass.getRamdomNumberInt(N);//产生0-100范围的数
 		
 		long startT = System.nanoTime();  
 		
@@ -14,7 +14,7 @@ public class Myclass {
 	    long endT = System.nanoTime();  
 		
 		for(int i=0;i<N;i++)
-			System.out.println((int)(A[i]*100));
+			System.out.println(A[i]);
 	
 	    System.out.println(N+"个数排序用时：" + (double)(endT - startT)/1000000+"毫秒");  
 	}
@@ -23,11 +23,11 @@ public class Myclass {
 		
 	}
 
-	public static double[] getRamdomNumber(int N) {
+	public static double[] getRamdomNumberInt(int N) {
 		N=Math.abs(N);
 		double []A= new double[N];
 		for(int i=0;i<N;i++){
-			A[i]=Math.random();
+			A[i]=(int)(Math.random()*100);
 		}
 		return A;
     }
