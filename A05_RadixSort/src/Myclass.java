77b8f6603bd6eac245e12d5cc87bd98527385fe4
zhongjuntian <zhongjun.tian@utdallas.edu>
2014-09-26 02:45:14 -0500

    0925

--- A05_RadixSort/src/Myclass.java
+++ A05_RadixSort/src/Myclass.java
@@ -40,31 +40,31 @@ public class Myclass {
 
 public static void radixsort2(int data[], int n) //基数排序
 {
-    int d = 3;
+    int d = 2;
     int[] tmp = new int[n];
-    int[] count = new int[8]; //计数器
+    int[] count = new int[16]; //计数器
     int i, j, k;
     int radix = 1;
-    for(i = 1; i <= d; i++) //进行d次排序
+    for(i = 1; i <= d; i++) //进行d次排序 (16+?)*2=32+2? (8+?)*3=24+3?  => ?=8 
     {
-        for(j = 0; j < 8; j++)
+        for(j = 0; j < 16; j++)
             count[j] = 0; //每次分配前清空计数器
-        for(j = 0; j < n; j++)//scan 8 times
+        for(j = 0; j < n; j++)//scan 16 times 
         {
-            k = (data[j] / radix) % 8; //统计每个桶中的记录数
+            k = (data[j] / radix) % 16; //统计每个桶中的记录数
             count[k]++;
         }
-        for(j = 1; j < 8; j++)
+        for(j = 1; j < 16; j++)
             count[j] = count[j - 1] + count[j]; //将tmp中的位置依次分配给每个桶
         for(j = n - 1; j >= 0; j--) //将所有桶中记录依次收集到tmp中
         {
-            k = (data[j] / radix) % 8;
+            k = (data[j] / radix) % 16;
             tmp[count[k] - 1] = data[j];
             count[k]--;
         }
         for(j = 0; j < n; j++) //将临时数组的内容复制到data中
             data[j] = tmp[j];
-        radix = radix * 8;
+        radix = radix * 16;
     }
 }
 	
