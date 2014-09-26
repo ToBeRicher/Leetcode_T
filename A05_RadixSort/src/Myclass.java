291f703cb9112f764a0fa17364a10d1751644298
zhongjuntian <zhongjun.tian@utdallas.edu>
2014-09-26 02:22:27 -0500

    0925

--- A05_RadixSort/src/Myclass.java
+++ A05_RadixSort/src/Myclass.java
@@ -4,28 +4,74 @@
 public class Myclass {
 	
 	public static void main(String[] args){		
-		int N=1000,range=255;
-		double[]A = Myclass.getRamdomNumberInt(N,range);//产生0-255范围的数
+		int N=1000000,range=255;
+		int[]A = Myclass.getRamdomNumberInt(N,range);//产生0-255范围的数
 		
 		long startT = System.nanoTime();  
-		
-		Myclass.Sort(A,N);
-		
+
+		int r=(int) (Math.log(range)/Math.log(2));
+		//Myclass.radixSort(A,N,r);
+		Myclass.radixsort2(A,N);
 	    long endT = System.nanoTime();  
 		
-		for(int i=0;i<N;i++)
-			System.out.println(A[i]);
+		//for(int i=0;i<N;i++)
+			//System.out.println(A[i]);
 	
 	    System.out.println(N+"个数排序用时：" + (double)(endT - startT)/1000000+"毫秒");  
 	}
 	
-	public static void Sort(double[] array,int length){
-		
+	public static void radixSort(int[] array,int length,int r){	
+		for(int shift=0;shift<=r;shift++){
+			int bit=1<<shift;
+			int i=-1;
+			for(int j=0;j<length;j++){//from start to end-1
+				int bb=(array[j]&bit);
+				if(bb ==0){
+					i++;
+					int temp=array[j]; //交换
+					array[j]=array[i];
+					array[i]=temp;
+				}
+			}
+		}
+		return;
 	}
+	
 
-	public static double[] getRamdomNumberInt(int N,int range) {
+public static void radixsort2(int data[], int n) //基数排序
+{
+    int d = 3;
+    int[] tmp = new int[n];
+    int[] count = new int[8]; //计数器
+    int i, j, k;
+    int radix = 1;
+    for(i = 1; i <= d; i++) //进行d次排序
+    {
+        for(j = 0; j < 8; j++)
+            count[j] = 0; //每次分配前清空计数器
+        for(j = 0; j < n; j++)//scan 8 times
+        {
+            k = (data[j] / radix) % 8; //统计每个桶中的记录数
+            count[k]++;
+        }
+        for(j = 1; j < 8; j++)
+            count[j] = count[j - 1] + count[j]; //将tmp中的位置依次分配给每个桶
+        for(j = n - 1; j >= 0; j--) //将所有桶中记录依次收集到tmp中
+        {
+            k = (data[j] / radix) % 8;
+            tmp[count[k] - 1] = data[j];
+            count[k]--;
+        }
+        for(j = 0; j < n; j++) //将临时数组的内容复制到data中
+            data[j] = tmp[j];
+        radix = radix * 8;
+    }
+}
+	
+
+	public static int[] getRamdomNumberInt(int N,int range) {
 		N=Math.abs(N);
-		double []A= new double[N];
+		int []A= new int[N];
 		for(int i=0;i<N;i++){
 			A[i]=(int)(Math.random()*range);
 		}
