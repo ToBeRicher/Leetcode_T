e0789397a6db56a3810bf83d2643ebebea8d6ddf
zhongjuntian <zhongjun.tian@utdallas.edu>
2014-11-06 00:36:41 -0600

    1106

---
+++ 011_Two_Sum/Myclass.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+class Myclass {
+ 
+
+	public static void main(String[] args){
+		Myclass m = new Myclass();
+		int[] numbers = {3,2,4};
+		int t = 6;
+		for(int i : m.twoSum(numbers,t))
+			System.out.println(i);
+	}
+	
+	public HashMap<Integer,Integer> ht = new HashMap<Integer,Integer>();
+    public int[] twoSum(int[] numbers, int target) {
+    	for(int i=0;i<numbers.length;i++){
+    		if(ht.get(target - numbers[i]) == null){
+    			ht.put(numbers[i], i);
+    		}else{
+    			int rs[] = {ht.get(target - numbers[i])+1,i+1};
+    			return rs;
+    		}
+    	}
+    	return null;
+    }
+}
